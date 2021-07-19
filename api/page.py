5c8fe5ac88fded6d1989c4a58f028ed2b2e8f4a4
Darasimi Ajewole <darasimi@formpl.us>
2021-07-19 07:57:47 +0100

    Set timeout of request

--- api/page.py
+++ api/page.py
@@ -2,6 +2,6 @@ from requests import get
 
 
 def get_page_content(request):
-    page_url = request.args.get('page_url')
-    response = get(page_url)
+    page_url = request.args.get('page_url', timeout=10)
+    response = get(page_url, )
     return response.content, response.status_code
