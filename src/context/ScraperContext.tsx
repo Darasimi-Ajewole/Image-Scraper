2b8d6bc84d22393bed36c62ab4527fc72e8fd89f
Darasimi Ajewole <darasimiajewole@gmail.com>
2022-05-02 17:29:03 +0100

    Migrated global context component to typescript

--- src/context/ScraperContext.tsx
+++ src/context/ScraperContext.tsx
@@ -1,15 +1,27 @@
 import React, { createContext, useState } from "react";
 import { fetchPage } from "../utils/dom";
-import pageParser  from "../utils/parser";
+import pageParser, { Image }  from "../utils/parser";
 
-export const ScraperContext = createContext();
+export const ScraperContext = createContext(null);
+
+export interface ScrapeResult {
+  success?: boolean,
+  status?: number,
+  errorMessage?: string
+}
+export interface ScraperContextInterface {
+  images: Image[],
+  pageUrl: string,
+  pageTitle: string,
+  runScrape: (pageUrl: string) => Promise<ScrapeResult>
+}
 
 const ScraperContextProvider = (props) => {
   const [images, setImages] = useState([]);
   const [pageUrl, setPageUrl] = useState('');
-  const [pageTitle, setPageTitle] = useState(null);
+  const [pageTitle, setPageTitle] = useState<string>(null);
 
-  const runScrape = async (pageUrl) => {
+  const runScrape = async (pageUrl: string): Promise<ScrapeResult> => {
     setPageUrl(pageUrl)
     const { pageContent, status } = await fetchPage(pageUrl);
     const success = String(status).startsWith('2')
